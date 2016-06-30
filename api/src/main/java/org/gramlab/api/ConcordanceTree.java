67b77ddcad3daab6bb41bfffe5c3859dbd96cd37
aleksandrachasch <chaschina.aleksandra@gmail.com>
2016-06-30 16:54:29 +0300

    Revert "Revert "initial commit for TreeCloud plugin""
    This reverts commit fef5fc2d323a6fda1f14d641bcea3e745014a33c.

---
+++ api/src/main/java/org/gramlab/api/ConcordanceTree.java
@@ -0,0 +1,19 @@
+package org.gramlab.api;
+
+import ro.fortsoft.pf4j.ExtensionPoint;
+
+/**
+ * Here is an ExtensionPoint for TreeCloud plugin
+ * @author Aleksandra Chashchina
+ *
+ */
+
+public interface ConcordanceTree extends ExtensionPoint{
+	
+	/*
+	 * here should be called a method that will return a JFrame with
+	 * a picture of the tree
+	 */
+	
+
+}
